3943614294af3bf40a14c6e563d0654cc89e77b8
dmitriikudrin <39126785+dmitriikudrin@users.noreply.github.com>
2020-07-12 21:48:51 +0700

    lesson 6

--- Урок 6. Обработка событий в JavaScript/exercise6.3/exercise6.3.js
+++ Урок 6. Обработка событий в JavaScript/exercise6.3/exercise6.3.js
@@ -34,40 +34,42 @@ let basketVisibility = {
     },
 
     buildBasket() {
-        for (i = 0; i++; i < 4) {
-            console.log((basketContents.content[i] !== undefined));
-            if (basketContents.content[i] !== undefined) {
+        $("div.basketRow").remove()
+        for (let el in basketContents.content) {
+            prod = basketContents.content[el]
+            if (basketContents.content[el] !== undefined) {
                 let basketRow = document.createElement('div');
                 basketRow.classList.add('basketRow');
                 basket.insertAdjacentElement('beforeend', basketRow);
 
                 let basketRowID = document.createElement('div');
                 basketRowID.classList.add('id');
-                basketRowID.innerText = `${basketContents.content[i].id}`;
-                basket.insertAdjacentElement('beforeend', basketRowID);
+                basketRowID.innerText = `${prod.id}`;
+                basketRow.insertAdjacentElement('beforeend', basketRowID);
 
                 let basketRowName = document.createElement('div');
-                basketRowName.classList.add('Name');
-                basketRowName.innerText = `${basketContents.content[i].name}`;
-                basket.insertAdjacentElement('beforeend', basketRowName);
+                basketRowName.classList.add('name');
+                basketRowName.innerText = `${prod.name}`;
+                basketRow.insertAdjacentElement('beforeend', basketRowName);
 
                 let basketRowPrice = document.createElement('div');
                 basketRowPrice.classList.add('price');
-                basketRowPrice.innerText = `${basketContents.content[i].price}`;
-                basket.insertAdjacentElement('beforeend', basketRowPrice);
+                basketRowPrice.innerText = `${prod.price}`;
+                basketRow.insertAdjacentElement('beforeend', basketRowPrice);
 
                 let basketRowCount = document.createElement('div');
                 basketRowCount.classList.add('count');
-                basketRowCount.innerText = `${basketContents.content[i].count}`;
-                basket.insertAdjacentElement('beforeend', basketRowCount);
+                basketRowCount.innerText = `${prod.count}`;
+                basketRow.insertAdjacentElement('beforeend', basketRowCount);
 
                 let basketRowDelete = document.createElement('div');
-                basketRowDelete.classList.add('fas fa-trash-alt delete');
-                basketRowDelete.setAttribute('data-id', '3');
-                basket.insertAdjacentElement('beforeend', basketRowDelete);
-
+                basketRowDelete.classList.add('fas', 'fa-trash-alt', 'delete');
+                basketRowDelete.setAttribute('data-id', `${prod.id}`);
+                basketRow.insertAdjacentElement('beforeend', basketRowDelete);
             }
         }
+
+        addDelBtnsListener();
     },
 }
 
@@ -78,12 +80,18 @@ buyBtns.forEach(function (buyBtn) {
         let id = event.target.dataset.id;
         let price = event.target.dataset.price;
         let name = event.target.dataset.name;
-        basketContents.addContent(id, price, name);
+        basketContents.addContent(id, name, price);
     })
 })
 
-
-
+function addDelBtnsListener() {
+    let delBtns = document.querySelectorAll('.delete');
+    delBtns.forEach(function (delBtn) {
+        delBtn.addEventListener('click', function (event) {
+            basketContents.delContent(event.target.dataset.id);
+        })
+    })
+}
 
 let basketContents = {
     content: {
@@ -101,6 +109,18 @@ let basketContents = {
         } else {
             this.content[id].count++
         }
+        basketVisibility.buildBasket()
+    },
+
+    delContent(id) {
+        console.log(this.content[id]);
+        console.log(this.content[id]);
+        if (this.content[id].count === 1) {
+            delete this.content[id];
+        } else {
+            this.content[id].count--;
+        }
+
         basketVisibility.buildBasket()
     },
 
