1f23e029d5ceb547c00aa4be4ea7bcba8cd3a26d
dmitriikudrin <dmitriikudrin@users.noreply.github.com>
2020-07-15 09:51:49 +0700

    Update exercise5.3.js

--- Урок 5. Введение в DOM/exercise5.3.js
+++ Урок 5. Введение в DOM/exercise5.3.js
@@ -1,6 +1,11 @@
 "use strict";
 
-
+let head = document.querySelector('head');
+head.insertAdjacentHTML('beforeend',
+    '<link\n' +
+    '    rel="stylesheet"\n' +
+    '    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"\n' +
+    '  />');
 
 
 let btn_open = document.getElementById("btn_open");
@@ -10,10 +15,22 @@ let modal_content = document.getElementById("modal_content");
 
 btn_open.onclick = function () {
     modal.style.display = "block";
+    modal_content.classList.add('animate__animated');
+    modal_content.classList.add('animate__backInDown');
+    setTimeout(function () {
+        modal_content.classList.remove('animate__animated');
+        modal_content.classList.remove('animate__backInDown');
+    }, 1000);
 }
 
 btn_close.onclick = function (event) {
-    modal.style.display = "none";
+    modal_content.classList.add('animate__animated');
+    modal_content.classList.add('animate__bounceOut');
+    setTimeout(function () {
+        modal_content.classList.remove('animate__animated');
+        modal_content.classList.remove('animate__bounceOut');
+        modal.style.display = "none";
+    }, 500);
 }
 
 modal.onclick = function () {
